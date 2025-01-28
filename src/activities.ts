a3b01b2b69ccddfc26ca5b9c587dda3e1f031669
ShakedEfraim <65243259+ShakedEfraim@users.noreply.github.com>
2025-01-28 17:13:39 +0200

    Initial commit

--- src/activities.ts
+++ src/activities.ts
@@ -1,4 +1,15 @@
-// Add Activity Definitions here.
-export async function YOUR_ACTIVITY(name: string): Promise<string> {
-  return `Hello, ${name}!`;
+import { promises } from 'node:dns';
+
+export async function getIP(): Promise<string>{
+  const url = 'https://icanhazip.com';
+  const response = await fetch(url);
+  const data = await response.text();
+  return data.trim();
+}
+
+export async function getLocationInfo(ip: string): Promise<string>{
+  const url = `http://ip-api.com/json/${ip}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return `${data.city}, ${data.regionName}, ${data.country}`
 }
--- src/workflows.ts
+++ src/workflows.ts
@@ -1,5 +1,26 @@
-// Add Workflow Definitions here.
-export async function YOUR_WORKFLOW(name: string): Promise<string> {
-  // Your workflow code here
-  return `Hello, ${name}!`;
+import * as workflow from '@temporalio/workflow';
+import type * as activities from './activities';
+
+const {getIP, getLocationInfo} = workflow.proxyActivities<typeof activities>({
+  retry: {
+    initialInterval: '1 second',
+    maximumInterval: '1 minute',
+    backoffCoefficient: 2,
+    maximumAttempts: 5
+  },
+  startToCloseTimeout: '1 minute'
+});
+
+export async function getAddressFromIP(name: string): Promise<string>{
+  try {
+    const ip = await getIP();
+    try {
+      const location = await getLocationInfo(ip);
+      return `Hello, ${name}. Your IP is ${ip} and your location is ${location}`;
+    } catch (error){
+      throw new workflow.ApplicationFailure("Failed to get location info");
+    }
+  }catch (error){
+    throw new workflow.ApplicationFailure("Failed to get IP");
+  }
 }
